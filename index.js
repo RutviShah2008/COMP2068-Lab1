fd963ee76681349e8758f4781aa3a065cb7ebb81
RutviShah2008 <200390373@student.georgianc.on.ca>
2019-05-06 20:55:37 -0400

    base for RockPaperSissor

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+  input: process.stdin, // Similar to Request in Node
+  output: process.stdout // Similar to response in Node
 });
+let validOptions = ['rock', 'paper', 'scissors'];
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const generateChoice = () => {
+  const index = Math.floor(Math.random() * Math.floor(3));
+  return validOptions[index];
+};
 
-  rl.close();
-});
+rl.question(
+  `Please choose one of the following:
+Rock
+Paper
+Scissors
+`,
+  userChoice => {
+    const choice = userChoice;
+    const computer = generateChoice();
+
+    // Winning Conditions
+    // First is Tie
+    if (choice === computer) {
+      console.log('There was a tie');
+    } else if (
+      // User winning conditions
+      (choice === 'rock' && computer === 'scissors') ||
+      (choice === 'scissors' && computer === 'paper') ||
+      (choice === 'paper' && computer === 'rock')
+    ) {
+      console.log('Player won');
+    } else {
+      // Else computer won
+      console.log('Computer won');
+    }
+
+    console.log(`Player chose: ${choice}
+    Computer chose: ${computer}`);
+
+    rl.close();
+  }
+);
